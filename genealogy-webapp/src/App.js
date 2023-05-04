cea7889a24c67afa92ff270c6c5e8bcf7d7a6ad9
hvn_ntdat <dat10hoa1@gmail.com>
2023-05-04 18:50:35 +0700

    know the graph works

--- genealogy-webapp/src/App.js
+++ genealogy-webapp/src/App.js
@@ -38,18 +38,29 @@ export default function App() {
 }
 
 function Graph() {
+	const {nodes, setNodes} = [];  
+
+	function onClickAddMem(currentNodes) {
+		let newNode = { id: 5, mid: 1, fid: 2, name: 'Datj', gender: 'female', img: 'https://cdn.balkan.app/shared/w10/3.jpg' };
+		currentNodes.push(newNode);
+	}
     return (
-        <div style={{height: '100%'}}>
-            <MyTree nodes={[
-                { id: 1, pids: [2], name: 'Amber McKenzie', gender: 'female', img: 'https://cdn.balkan.app/shared/2.jpg'  },
-                { id: 2, pids: [1], name: 'Ava Field', gender: 'male', img: 'https://cdn.balkan.app/shared/m30/5.jpg' },
-                { id: 3, mid: 1, fid: 2, name: 'Peter Stevens', gender: 'male', img: 'https://cdn.balkan.app/shared/m10/2.jpg' },
-                { id: 4, mid: 1, fid: 2, name: 'Savin Stevens', gender: 'male', img: 'https://cdn.balkan.app/shared/m10/1.jpg'  },
-                { id: 5, mid: 1, fid: 2, name: 'Emma Stevens', gender: 'female', img: 'https://cdn.balkan.app/shared/w10/3.jpg' }
-            ]} />
-        </div>
+		<>
+			<div style={{height: '100%'}}>
+				<MyTree nodes={[
+					{ id: 1, pids: [2], name: 'Amber McKenzie', gender: 'female', img: 'https://cdn.balkan.app/shared/2.jpg'  },
+					{ id: 2, pids: [1], name: 'Ava Field', gender: 'male', img: 'https://cdn.balkan.app/shared/m30/5.jpg' },
+					{ id: 3, mid: 1, fid: 2, name: 'Peter Stevens', gender: 'male', img: 'https://cdn.balkan.app/shared/m10/2.jpg' },
+					{ id: 4, mid: 1, fid: 2, name: 'Savin Stevens', gender: 'male', img: 'https://cdn.balkan.app/shared/m10/1.jpg'  },
+					{ id: 5, mid: 1, fid: 2, name: 'Emma Stevens', gender: 'female', img: 'https://cdn.balkan.app/shared/w10/3.jpg' },
+				]} />
+			</div>
+			<button onClick={onClickAddMem}>Add mem</button>
+		</>
     );
 }
+
+
 
 // function Tree() {
 // 	return (
